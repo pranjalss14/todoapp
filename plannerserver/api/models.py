aa70858b05451666deef6087fdecf1ebffada5e3
Pranjal <pranjalss14@gmail.com>
2024-07-30 19:33:09 +0530

    Your commit message

--- plannerserver/api/models.py
+++ plannerserver/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class ticket(models.model)
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255, null=False)
+    description = models.TextField()
+    status = models.CharField(max_length=255, null=False)
+    priority = models.CharField(max_length=255, null=False)
+    owner = models.CharField(max_length=255, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+
+
